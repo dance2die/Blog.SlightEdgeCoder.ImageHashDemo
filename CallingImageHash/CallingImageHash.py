a8a4f194779f06a48402c5cd8d889e0803582f16
dance2die <dontbothermewithspam@gmail.com>
2016-12-26 14:39:39 -0500

    Able to call ImageHash from IronPython

--- CallingImageHash/CallingImageHash.py
+++ CallingImageHash/CallingImageHash.py
@@ -1,4 +1,9 @@
-from PIL import Image
+import PIL.Image
+from pprint import pprint as pp
 import imagehash
 
-hash = imagehash.average_hash(Image.Open(''))
+hash1 = imagehash.dhash(PIL.Image.open('images/a.jpg'))
+hash2 = imagehash.dhash(PIL.Image.open('images/b.jpg'))
+
+print("hash1 = {}, hash2 = {}, hash1 - hash2 = {}".format(hash1, hash2, hash1 - hash2))
+
